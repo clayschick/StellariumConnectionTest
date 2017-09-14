f728fe5c545acfbd24fba7b16455c16ead313a96
Clay Schick <clay.schick@gmail.com>
2017-09-14 11:44:13 -0500

    Basic TCP listener that is accepting connections

--- StellariumConnectionTest/Program.fs
+++ StellariumConnectionTest/Program.fs
@@ -1,69 +1,49 @@
 ﻿module Program
 
 open System
+open System.IO
 open System.Net
 open System.Net.Sockets
+open System.Threading
 
-let rec asyncPrintResponse (stream : NetworkStream) =
-    async {
-        let response = stream.ReadByte() |> Char.ConvertFromUtf32 //<-- not sure about this format
-        Console.Write(response)
-        printf "response : %A" |> ignore
-        return! asyncPrintResponse stream
-    }
+// System.Net.Sockets.Socket extension methods
+type Socket with
+    member socket.AsyncAccept() = Async.FromBeginEnd(socket.BeginAccept, socket.EndAccept)
 
+type Server() =
+    static member start() =
+        let ipAddr = IPAddress.Parse("192.168.1.71") 
+        let endpoint = IPEndPoint(ipAddr, 10001)
+        let cts = new CancellationTokenSource()
 
-let AsyncAcceptClients(listener : TcpListener) =
-  async {
-    let controlVar = true
-    while controlVar do
-        let! result = Async.Catch listener.AcceptTcpClient
-        match result with
-        | Choice1Of2 result -> 
-            match result with
-                | None -> ignore()
-                | Some client -> Async.Start <| AsyncAcceptMessages client
-        | Choice2Of2 error -> 
-            Log [|"Error Accepting clients: "; error.Message|]
-    Log "Server Stop accepting clients"
-  }
+        let listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+        listener.Bind(endpoint)
+        listener.Listen(int SocketOptionName.MaxConnections)
 
-let endpoint = new IPEndPoint(IPAddress.Parse("192.168.1.71"),10001)
+        printfn "Server listening on port %d" 10001
 
-use listener = new TcpListener(endpoint)
+        let rec loop() = async {
+            printfn "Waiting for requests"
+            let! socket = listener.AsyncAccept()
+            printfn "Received connection request"
+
+            return! loop()
+        }
+
+        Async.Start(loop(), cancellationToken = cts.Token)
+        { new IDisposable with member x.Dispose() = cts.Cancel(); listener.Close() }
+ 
 
 [<EntryPoint>]
 let main argv = 
-    listener.Start()
 
-    Console.ReadKey true |> ignore
-    listener.Stop()
+    let disposable = Server.start()
 
-    let controlVar = true
+    printfn "Hit enter to continue"
+    Console.Read() |> ignore
 
-    //while controlVar do
+    printfn "Disposing of server"
 
-
-    // This is only client side stuff. I need to listen as a server.
-    //let client = new System.Net.Sockets.TcpClient()
-    //client.Connect("192.168.1.71", 8090)
-    //let stream = client.GetStream()
-    //asyncPrintResponse stream |> Async.RunSynchronously
-
-    // The telescope control plug-in uses websockets to talk. It connects to us.
-    // Create a websocket listener as an observable
-    // https://stackoverflow.com/questions/39112800/observing-incoming-websocket-messages-with-reactive-extensions
-
-
-    // Passing in a Sceduler for ThreadPool access
-    //let obs = intervalOn Scheduler.Default (TimeSpan.FromSeconds(2.))
-
-    //obs 
-    //|> subscribe(fun i -> printfn "Doing stuff %i" i) 
-    //|> ignore
-
-    Console.WriteLine("\nHit enter to continue...");
-    Console.Read();
-
+    disposable.Dispose()
     0 // return an integer exit code
 
